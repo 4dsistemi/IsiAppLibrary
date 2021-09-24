cd64d0dd81cd98e16e04d5c941ba9f6635d99bca
edoardo <edoardogullo@libero.it>
2021-09-24 12:24:49 +0200

    Nuova schermata customer

--- isilibrary/src/main/java/com/isi/isilibrary/backActivity/BackActivity.java
+++ isilibrary/src/main/java/com/isi/isilibrary/backActivity/BackActivity.java
@@ -1,10 +1,17 @@
 package com.isi.isilibrary.backActivity;
 
+import android.graphics.Color;
 import android.view.MenuItem;
+import android.widget.Button;
 
+import androidx.annotation.CallSuper;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.isi.isilibrary.R;
+
 import java.util.Objects;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class BackActivity extends AppCompatActivity {
 
@@ -24,5 +31,48 @@ public class BackActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @CallSuper
+    public void updateUI(int layout){
+        runOnUiThread(() -> setContentView(layout));
+    }
+
+
+    public void errorPage(int layout){
+        runOnUiThread(() -> {
+            setContentView(R.layout.error_data);
+
+            Button reloadButton = findViewById(R.id.reload_data_button);
+
+            reloadButton.setOnClickListener(v -> updateUI(layout));
+        });
+
+    }
+
+    public void emptyData(){
+        runOnUiThread(() -> setContentView(R.layout.empty_data));
+    }
+
+    private SweetAlertDialog pDialog;
+
+    public void startLoader(String title){
+        final String titleIn = title == null ? "Aggiorno dati..." : title;
+        runOnUiThread(() -> {
+            pDialog = new SweetAlertDialog(BackActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+            pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+            pDialog.setTitleText(titleIn);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        });
+
+    }
+
+    public void stopLoader(){
+        runOnUiThread(() -> {
+            if(pDialog != null){
+                pDialog.dismissWithAnimation();
+            }
+        });
     }
 }
